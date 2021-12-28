bad75733d4c0de65eb392577895a53ed4c2905b9
arbiter <arbiterqq@gmail.com>
2021-12-28 15:08:12 +0800

    add MedicArchetype

---
+++ src/main/java/com/example/ttdev/dpsSmu/enums/MedicArchetype.java
@@ -0,0 +1,24 @@
+package com.example.ttdev.dpsSmu.enums;
+/**
+ * 医疗细分职业
+ *
+ * @author linyifei1998
+ */
+public enum MedicArchetype {
+    /**
+     * 元素奶 2.85
+     */
+    ElementHeal,
+    /**
+     * 单奶 2.85
+     */
+    SingleHeal,
+    /**
+     * 群奶 2.85
+     */
+    AoEHeal,
+    /**
+     * 远奶 2.85
+     */
+    LoneHeal
+}
